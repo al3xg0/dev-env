9bff140abd9e73df0dd071a2e73ce5272fcc31a9
alexguja <al.guja@outlook.com>
2026-05-03 14:22:11 +0200

    fix diffview display

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -4,7 +4,6 @@ return {
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
   config = function()
     require("diffview").setup()
-    vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#ffdce0" })
 
     vim.keymap.set("n", "<leader>dv", ":DiffviewOpen<CR>", { desc = "Open Diffview" })
     vim.keymap.set("n", "<leader>gq", ":DiffviewClose<CR>", { desc = "Close Diffview" })
